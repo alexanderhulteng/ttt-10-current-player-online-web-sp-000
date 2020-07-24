3b277f951b2ee5993a1ec43cf3c5153bd7a977ec
Alexander Hulteng <alexanderhulteng@gmail.com>
2020-07-24 00:31:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,7 +5,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def turn_count(board)
   turns = 0
   board.each do |space|
-    if space == "X"
+    if space == "X" || space == "O"
       turns += 1
     end
   end
